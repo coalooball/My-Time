53bc323d87a50f76bab4b9c27b95d242c58f77c5
coalooball <cyan_cg@outlook.com>
2024-05-27 14:22:49 +0800

    .

--- my_sql.sql
+++ my_sql.sql
@@ -8,3 +8,5 @@ GROUP BY
     description
 ORDER BY 
     total_time_hours DESC;
+
+select category, description, detail, start_time, end_time, total_time_seconds/3600.0 as hours from timer WHERE start_time >= DATE('now', '-1 day');
