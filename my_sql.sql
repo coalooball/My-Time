afc309b6aceab1f6891101697fb7c12023bc0038
coalooball <cyan_cg@outlook.com>
2024-06-03 15:38:19 +0800

    .

--- my_sql.sql
+++ my_sql.sql
@@ -10,3 +10,15 @@ ORDER BY
     total_time_hours DESC;
 
 select category, description, detail, start_time, end_time, total_time_seconds/3600.0 as hours from timer WHERE start_time >= DATE('now', '-1 day');
+
+SELECT 
+	DISTINCT(category), 
+    description, 
+    SUM(total_time_seconds) / 3600.0 as total_time_hours 
+FROM 
+    timer
+WHERE start_time >= DATE('now', '-1 day')
+GROUP BY 
+    description
+ORDER BY 
+    total_time_hours DESC;
